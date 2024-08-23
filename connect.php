8e917e7a2b2663481c9fa22fe0acc0a662a3a35b
giabao123789 <giabao2461@gmail.com>
2024-08-24 00:04:32 +0700

    giabao

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+$host = 'localhost';
+$name = 'root';
+$password ='';
+$dbname = 'data nha hàng';
+$conn = new mysqli($host,$name,$password,$dbname);
+if ($conn -> connect_error){
+    die("KẾT NỐI KHÔNG THÀNH CÔNG !".$conn -> connect_error);
+}
+
+
+
+
+
+
+?>
--- submit_form.php
+++ submit_form.php
@@ -1,6 +1,11 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+   
+
+
 <?php
-// Kiểm tra nếu yêu cầu đến là POST
+ require 'connect.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Nhận dữ liệu từ biểu mẫu
     $name = htmlspecialchars($_POST['name']);
@@ -10,14 +15,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = htmlspecialchars($_POST['message']);
 
     // Hiển thị thông tin đã nhận được
-    echo "<h2>Thông tin bạn đã gửi:</h2>";
+    echo "<h1>Thông tin bạn đã gửi:</h1>";
     echo "Họ và tên: " . $name . "<br>";
     echo "Email: " . $email . "<br>";
     echo "Số điện thoại: " . $phone . "<br>";
     echo "Chủ đề: " . $subject . "<br>";
     echo "Tin nhắn: " . $message . "<br>";
+
+    $sql ="INSERT INTO `user` (`name`,`email`,`phone`,`subject`,`message`) VALUES('$name','$email','$phone','$subject','$message')";
+    if($conn ->query($sql) === TRUE ){ 
+        echo"<h1>CHÚNG TÔI SẼ LIÊN HỆ CHO BẠN TRONG VÒNG 24H ĐỂ TƯ VẤN NHÉ! XIN CẢM ƠN QUÝ KHÁCH!</h1>";
+    }
+    else {
+        echo"Lỗi {$sql}".$connn -> error;
+    }
 } else {
     // Hiển thị thông báo nếu yêu cầu không phải là POST
     echo "Yêu cầu không hợp lệ.";
 }
 ?>
+<A href="code.html">QUAY LẠI TRANG CHỦ CALLARY</A>
